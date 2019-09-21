9a54dc71d19e5884000a00e6c584f37d0d46873e
Micah <micah.d.lamb@gmail.com>
2019-09-21 12:54:27 -0400

    create secrets.sh to hold twilio auth

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'
-#xdg-open http://localhost:5000 &
+#xdg-open http://localhost &
+source secrets.sh
 pipenv run python app.py
