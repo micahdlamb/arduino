dc7a3c0b510009ce3fbcfce7b5e7219e062175d0
Micah <micah.d.lamb@gmail.com>
2019-09-22 21:29:43 -0400

    Show pressure sensors

--- react-reduction/src/pages/SensorsPage.js
+++ react-reduction/src/pages/SensorsPage.js
@@ -4,11 +4,11 @@ import * as rs from 'reactstrap';
 import * as fa from 'react-icons/fa';
 // import { getColor } from 'utils/colors';
 import hotIcon from 'assets/img/hot.png';
+import pressureIcon from 'assets/img/pressure.png'
 import ReadPin from 'components/ReadPin';
-import TogglePin from 'components/TogglePin';
 import { connect } from 'react-redux';
 import {notify} from 'store';
-import {chiller1, chiller2} from 'chips';
+import {chiller1, chiller2, lp, hp} from 'chips';
 import EditSetting from 'components/EditSetting';
 
 
@@ -22,6 +22,12 @@ export default function SensorsPage(){
         </rs.Col>
         <rs.Col xl={6} lg={6} md={12}>
           <TemperatureCard chip={chiller2} pin='T1'/>
+        </rs.Col>
+        <rs.Col xl={6} lg={6} md={12}>
+          <PressureCard chip={lp} pin='1'/>
+        </rs.Col>
+        <rs.Col xl={6} lg={6} md={12}>
+          <PressureCard chip={hp} pin='1'/>
         </rs.Col>
       </rs.Row>
     </Page>
@@ -39,14 +45,15 @@ let TemperatureCard = ({notify, chip, pin, maxTemp}) => {
           message: `${chip.label} got hot`,
         })
     }
-    return <span className={alert && 'warn-text'}>{value.toFixed(1)}° F</span>
+    return <span className={alert && 'warn-text'}>{value.toFixed(1)}° <small>F</small></span>
   }
 
   return (
     <rs.Card>
       <rs.CardHeader>
-        <h3 className="d-none d-sm-block">{chip.label}</h3>
-        <h3 className="d-sm-none"><fa.FaRobot/></h3>
+        <h3>{chip.label}</h3>
+        {/* <h3 className="d-none d-sm-block">{chip.label}</h3>
+        <h3 className="d-sm-none"><fa.FaRobot/></h3> */}
         <div>
           <rs.InputGroup>
             <EditSetting
@@ -62,7 +69,6 @@ let TemperatureCard = ({notify, chip, pin, maxTemp}) => {
             </rs.InputGroupAddon>
           </rs.InputGroup>
         </div>
-        <TogglePin chip={chip} pin={6} />
       </rs.CardHeader>
       <rs.CardBody className='text-center text-nowrap' style={{fontSize: '10vw'}}>
         <ReadPin chip={chip} pin={pin} render={temperature}/>
@@ -72,3 +78,46 @@ let TemperatureCard = ({notify, chip, pin, maxTemp}) => {
 }
 
 TemperatureCard = connect(({settings}) => ({maxTemp: settings.maxTemp}), { notify })(TemperatureCard)
+
+
+let PressureCard = ({notify, chip, pin, maxPressure}) => {
+  let pressure = value => {
+    if (maxPressure != null){
+      var alert = value >= maxPressure || null
+      if (alert)
+        notify({
+          avatar: pressureIcon,
+          message: `${chip.label} got high`,
+        })
+    }
+    return <span className={alert && 'warn-text'}>{value.toFixed(1)} <small>PSI</small></span>
+  }
+
+  return (
+    <rs.Card>
+      <rs.CardHeader>
+        <h3>{chip.label}</h3>
+        <div>
+          <rs.InputGroup>
+            <EditSetting
+              setting='maxPressure'
+              type='number'
+              placeholder="Max PSI"
+              style={{maxWidth: '11ch'}}
+            />
+            <rs.InputGroupAddon addonType="append">
+              <rs.InputGroupText>
+                <fa.FaWeight/>
+              </rs.InputGroupText>
+            </rs.InputGroupAddon>
+          </rs.InputGroup>
+        </div>
+      </rs.CardHeader>
+      <rs.CardBody className='text-center text-nowrap' style={{fontSize: '10vw'}}>
+        <ReadPin chip={chip} pin={pin} render={pressure}/>
+      </rs.CardBody>
+    </rs.Card>
+  )
+}
+
+PressureCard = connect(({settings}) => ({maxPressure: settings.maxPressure}), { notify })(PressureCard)
